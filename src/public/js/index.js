501f09bd7ebc84364553f5fa9fc9f2d6add66066
Rachit gupta <rachchooit@gmail.com>
2023-04-20 11:30:46 +0530

    fix: typo

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -30,4 +30,4 @@ async function outputTechs(url) {
 
 // async function outputTechUsers(userUrl, tech) {}
 
-outputTechs(`https://p2p-communication.netlify.app/getTechUsers`);
+outputTechs(`https://p2p-communication.netlify.app/getTechNames`);
